b2f7fb4e39282354d9ed319baaa16662c65f24da
Lucas Nicollier <lucas.nicollier@gmail.com>
2023-08-26 16:47:01 +0800

    changed the logic of the unit test, they're now running throught all value and compare the return value between ft_foo adnd foo

--- include/libft.h
+++ include/libft.h
@@ -4,6 +4,7 @@
     int ft_isalnum(int c);
     int ft_isalpha(char c);
     int ft_isascii(int c);
+    int ft_isprint(int c);
     int ft_isdigit(int c);
     size_t ft_strlen(const char *s, size_t maxlen);
     void *ft_memset(void *s, int c, size_t len);
---
+++ tests/test_libft.cpp
@@ -0,0 +1,62 @@
+extern "C" {
+    #include "libft.h"
+    #include <stdlib.h>
+}
+
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(FTIsdigit) {};
+
+//unit test for: isalpha
+TEST(FTIsdigit, FTisalpha) {
+    for (int i = 0; i < 256; i++) {
+        CHECK(ft_isalpha(i) == isalpha(i));
+    }
+}
+
+//unit test for: isdigit
+TEST(FTIsdigit, FTisdigit) {
+    for (int i = 0; i < 256; i++) {
+        CHECK(ft_isdigit(i) == isdigit(i));
+    }
+}
+
+//unit test for: isalnum
+TEST(FTIsdigit, FTisalnum) {
+    for (int i = 0; i < 256; i++) {
+        CHECK(ft_isalnum(i) == isalnum(i));
+    }
+}
+
+//unit test for: isascii
+    TEST(FTIsdigit, FTisascii) {
+    for (int i = 0; i < 256; i++) {
+        CHECK(ft_isascii(i) == isascii(i));
+    }
+}
+
+//unit test for: isprint
+TEST(FTIsdigit, FTisprint) {
+    for (int i = 0; i < 256; i++) {
+        CHECK(ft_isprint(i) == isprint(i));
+    }
+}
+
+//unit test for: strlen
+//unit test for: memset
+//unit test for: bzer
+//unit test for: memcpy
+//unit test for: memmove
+//unit test for: strlcpy
+//unit test for: strlcat
+//unit test for: toupper
+//unit test for: tolower
+//unit test for: strchr
+//unit test for: strrchr
+//unit test for: strncmp
+//unit test for: memchr
+//unit test for: memcmp
+//unit test for: strnstr
+//unit test for: atoi
+//unit test for: calloc
+//unit test for: strup
