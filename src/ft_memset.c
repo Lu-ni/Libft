1a3774584de045a40210141b9dd8fc71f2cfbb20
Lucas Nicollier <lucas.nicollier@gmail.com>
2023-08-27 14:37:28 +0800

    add test for everything up to memset, everzthing seems to work fine, but no edge case tested yet. This, mostly, setup the unit test environement

--- src/ft_memset.c
+++ src/ft_memset.c
@@ -1,13 +1,13 @@
 #include "libft.h"
 
-void *memset(void *b, int c, size_t len)
+void *ft_memset(void *b, int c, size_t len)
 {
     char *str;
 
-    str = (char *)b;
-    while (len-- > 0)
-    {
-        *str = c;
-    }
+   str = (char *)b;
+   while (len-- > 0)
+   {
+       *str++ = c;
+   }
     return (b);
 }
--- tests/test_libft.cpp
+++ tests/test_libft.cpp
@@ -5,16 +5,17 @@ extern "C" {
 
 #include "CppUTest/TestHarness.h"
 
-TEST_GROUP(FTt) {};
+TEST_GROUP(Ft) {};
 
 //unit test for: isalpha
-TEST(FTt, FTisalpha) {
+TEST(Ft, FTisalpha) {
     for (int i = 0; i < 256; i++) {
         CHECK(ft_isalpha(i) == isalpha(i));
     }
 }
 
 //unit test for: isdigit
+TEST_GROUP(FTt) {};
 TEST(FTt, FTisdigit) {
     for (int i = 0; i < 256; i++) {
         CHECK(ft_isdigit(i) == isdigit(i));
@@ -30,20 +31,39 @@ TEST(FTt, FTisalnum) {
 
 //unit test for: isascii
     TEST(FTt, FTisascii) {
-    for (int i = 0; i < 256; i++) {
+    for (int i = -1000; i < 2560; i++) {
         CHECK(ft_isascii(i) == isascii(i));
     }
 }
 
 //unit test for: isprint
 TEST(FTt, FTisprint) {
-    for (int i = 0; i < 256; i++) {
+    for (int i = -1000; i < 2560; i++) {
         CHECK(ft_isprint(i) == isprint(i));
     }
 }
 
 //unit test for: strlen
+TEST(FTt, FTstrlen) {
+    CHECK(ft_strlen("") == strlen(""));
+    CHECK(ft_strlen("ola") == strlen("ola"));
+    CHECK(ft_strlen("923084 owewijr ;';';,.,.") == strlen("923084 owewijr ;';';,.,."));
+    CHECK(ft_strlen("!@#!@#$%^i*(^%)$") == strlen("!@#!@#$%^i*(^%)$"));
+}
+
 //unit test for: memset
+TEST(FTt, FTmemset) {
+    char str1[10] = {'a','a','a','a','a','a','a','a','\0'};
+    char str2[10] = {'a','a','a','a','a','a','a','a','\0'};
+
+    ft_memset(str1, 'x', 4);
+    memset(str2, 'x', 4);
+    STRCMP_EQUAL(str2,str1);
+
+    ft_memset(str1 + 2, 'e', 3);
+    memset(str2 + 2, 'e', 3);
+    STRCMP_EQUAL(str2,str1);
+}
 //unit test for: bzer
 //unit test for: memcpy
 //unit test for: memmove
